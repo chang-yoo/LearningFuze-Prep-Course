801bb26425850f279c5601157723492fe9803d15
Changhyun Yoo <chyoo0428@gmail.com>
2022-03-04 01:01:56 +0000

    Completed prep-javascript-objects

---
+++ prep-javascript-objects/main.js
@@ -0,0 +1,13 @@
+var person = {
+  firstName: 'changhyun',
+  lastName: 'Yoo',
+  hobby: 'Game'
+};
+console.log("The Person's name is: ", person.firstName);
+console.log(person.lastName);
+console.log(person.hobby);
+person.job = 'production';
+console.log("The person's current job is: ", person.job);
+person.previousJob = 'student';
+console.log("The person's previous job was: ", person.previousJob);
+console.log(person);
